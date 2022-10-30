b0362a0dcce5d63f0a183b0f7f60629395ccad55
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2022-10-30 20:37:11 +0200

    update promises-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,54 +1,42 @@
 import Notiflix from 'notiflix';
 
 const formRef = document.querySelector('.form');
-console.log(formRef);
-const btnRef = document.querySelector('button');
-const inputDelay = document.querySelector('[name=delay]');
-console.log(inputDelay);
-const inputStep = document.querySelector('[name=step]');
-const inputAmount = document.querySelector('[name=amount]');
 
-formRef.addEventListener('submit', createPromise);
-// // let position =
-let delay = inputDelay.value;
-let AMOUNT = inputAmount.value;
-console.log(AMOUNT);
-console.log(delay);
+formRef.addEventListener('submit', onSubmit);
 
-// let timerId = null;
-function createPromise(position, delay) {
-        // event.preventDefault();
-    console.log({ position: inputAmount.value, delay: inputDelay.value });
-    return new Promise((resolve, reject) => {
-      const shouldResolve = Math.random() > 0.3;
-      timerId = setInterval(() => {
-      if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      }}, AMOUNT)
-    })
+function onSubmit(event) {
+  event.preventDefault();
+  const formElements = new FormData(event.currentTarget);
+  const dataObj = {};
+  formElements.forEach((value, name) => (dataObj[name] = value));
+  let delayValue = Number(dataObj.delay);
+  let amountValue = Number(dataObj.amount);
+  let stepValue = Number(dataObj.step);
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += stepValue;
+  }
 }
 
-
-
-// const promise = new Promise((resolve, reject) => { };
-//     let random = Math.random() > .5
-// setTimeout(()=>{
-//     if(random) {
-//         cake = 'Тортик спекли :3'
-//         resolve(cake)
-//     }else{
-//         cake = 'Тортик провтикали :с'
-//         reject(cake)
-//     }
-// }, 2000)
-// })
-//
-createPromise(position, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+}
